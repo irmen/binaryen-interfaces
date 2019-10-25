c35302295445e6c877dff886aa386a66f798cf50
Irmen de Jong <irmen@razorvine.net>
2019-10-25 02:35:19 +0200

    new version

--- pybinaryen/tests/test_binaryen.py
+++ pybinaryen/tests/test_binaryen.py
@@ -1,18 +1,18 @@
 import unittest
-import binaryen as b
+import binaryen
 
 
 class TestBinaryen(unittest.TestCase):
     def test_basic(self):
-        module = b.ModuleCreate()
-        params = [b.TypeInt32(), b.TypeInt32()]
-        iii = b.AddFunctionType(module, b"iii", b.TypeInt32(), params, len(params))
-        x = b.LocalGet(module, 0, b.TypeInt32())
-        y = b.LocalGet(module, 1, b.TypeInt32())
-        add = b.Binary(module, b.AddInt32(), x, y)
-        adder = b.AddFunction(module, b"adder", iii, b.ffi.NULL, 0, add)
-        b.ModulePrint(module)
-        b.ModuleDispose(module)
+        module = binaryen.ModuleCreate()
+        params = [binaryen.TypeInt32(), binaryen.TypeInt32()]
+        iii = binaryen.AddFunctionType(module, b"iii", binaryen.TypeInt32(), params, len(params))
+        x = binaryen.LocalGet(module, 0, binaryen.TypeInt32())
+        y = binaryen.LocalGet(module, 1, binaryen.TypeInt32())
+        add = binaryen.Binary(module, binaryen.AddInt32(), x, y)
+        adder = binaryen.AddFunction(module, b"adder", iii, binaryen.ffi.NULL, 0, add)
+        binaryen.ModulePrint(module)
+        binaryen.ModuleDispose(module)
 
 
 if __name__ == '__main__':
